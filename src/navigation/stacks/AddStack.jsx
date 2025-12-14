f316ad7669aabf03ba0a1b79c654d3b879e649b4
Tushardawar <tushardawar446@gmail.com>
2025-12-14 14:20:10 -0700

    Update AddStack.jsx

---
+++ src/navigation/stacks/AddStack.jsx
@@ -0,0 +1,24 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AddAssignmentScreen from "../../features/assignments/screens/AddAssignmentScreen";
+import BackButton from "../../shared/components/BackButton";
+
+const Stack = createNativeStackNavigator();
+
+export default function AddStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTitleAlign: "center",
+      }}
+    >
+      <Stack.Screen
+        name="AddAssignmentMain"
+        component={AddAssignmentScreen}
+        options={({ navigation }) => ({
+          title: "Add Assignment",
+          headerLeft: () => <BackButton navigation={navigation} />,
+        })}
+      />
+    </Stack.Navigator>
+  );
+}
