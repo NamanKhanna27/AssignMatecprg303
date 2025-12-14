54e7b9755f44814768f22e77e4a84b518d02c174
NamanKhanna27 <namank27102004@gmail.com>
2025-12-14 16:45:55 -0700

    Create AppProvider.jsx

---
+++ src/app/AppProviders.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import RootNavigator from "../navigation/RootNavigator";
+import { AuthFlowProvider } from "../features/auth/authFlow/AuthFlowContext";
+import { AppDialogProvider } from "../shared/components/AppDialog/AppDialogContext";
+
+export default function AppProviders() {
+  return (
+    <AuthFlowProvider>
+      <AppDialogProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </AppDialogProvider>
+    </AuthFlowProvider>
+  );
+}
