6dd0f7a7861db17840eaace77844e533789fce71
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-12-14 14:29:41 -0600

    Update ProfileStack.jsx

---
+++ src/navigation/stacks/ProfileStack.jsx
@@ -0,0 +1,35 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ProfileScreen from "../../features/profile/screens/ProfileScreen";
+import EditProfileScreen from "../../features/profile/screens/EditProfileScreen";
+import BackButton from "../../shared/components/BackButton";
+
+const Stack = createNativeStackNavigator();
+
+export default function ProfileStack({ setIsLoggedIn }) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTitleAlign: "center",
+      }}
+    >
+      <Stack.Screen
+        name="ProfileMain"
+        options={({ navigation }) => ({
+          title: "Profile",
+          headerLeft: () => <BackButton navigation={navigation} />,
+        })}
+      >
+        {(props) => <ProfileScreen {...props} setIsLoggedIn={setIsLoggedIn} />}
+      </Stack.Screen>
+
+      <Stack.Screen
+        name="EditProfile"
+        component={EditProfileScreen}
+        options={({ navigation }) => ({
+          title: "Edit Profile",
+          headerLeft: () => <BackButton navigation={navigation} />,
+        })}
+      />
+    </Stack.Navigator>
+  );
+}
